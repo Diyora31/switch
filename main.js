8af26600210947b35f0599a6d39ce047669b96ad
Diyora31 <asilovad7>
2024-05-20 11:59:09 +0500

    done

---
+++ main.js
@@ -0,0 +1,52 @@
+let month = prompt('write name of the month')
+
+
+switch (month) {
+    case 'dekabr': console.log('winter'); break
+    case 'yanvar': console.log('winter'); break
+    case 'fevral': console.log('winter'); break
+    case 'mart': console.log('spring'); break
+    case 'aprel': console.log('spring'); break
+    case 'may': console.log('spring'); break
+    case 'iyun': console.log('summer'); break
+    case 'iyul': console.log('summer'); break
+    case 'avgust': console.log('summer'); break
+    case 'sentabr': console.log('autumn'); break
+    case 'oktabr': console.log('autumn'); break
+    case 'noyabr': console.log('autumn'); break
+
+    default: console.log('unaqa oy yoq');
+}
+
+
+
+// let minuteprompt = +prompt('minut yozin')
+
+// let sutka = Math.floor(minuteprompt / 1440)
+// let sutka2 = minuteprompt % 1440
+// let hour = Math.floor(sutka2 / 60)
+// let minute = sutka2 % 60
+// console.log(`'sutka'${sutka}  , 'soat'${hour} , 'minut'${minute}`);
+
+
+
+// let day = +prompt(' 1 dan 7 gacha son kiriting')
+
+// switch (day) {
+//     case '1': console.log('Dushanba'); break
+//     case '2': console.log('Seshanba'); break
+//     case '3': console.log('Chorshanba'); break
+//     case '4': console.log('Payshanba'); break
+//     case '5': console.log('Juma'); break
+//     case '6': console.log('Shanba'); break
+//     case '7': console.log('Yakshanba'); break
+
+
+//     // default:console.log('Unaqa kun yoq');
+// }
+
+
+
+
+
+
